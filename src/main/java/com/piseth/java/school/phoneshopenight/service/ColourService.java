d7e1bb7264f0e6d0a62097a10b5779b741df83c0
Kosal Nim <kosall123nim@gmail.com>
2024-12-04 05:37:20 +0700

    create colourService

---
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/ColourService.java
@@ -0,0 +1,8 @@
+package com.piseth.java.school.phoneshopenight.service;
+
+import com.piseth.java.school.phoneshopenight.entity.Colour;
+
+public interface ColourService {
+	Colour build(Colour colour);
+
+}
