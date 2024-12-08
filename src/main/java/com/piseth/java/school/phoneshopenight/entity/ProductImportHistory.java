c7fddb9eeb10bd289bea7f3be53f509e5213844c
Kosal Nim <kosall123nim@gmail.com>
2024-12-08 19:03:30 +0700

    create ProductImportHistory.java

---
+++ src/main/java/com/piseth/java/school/phoneshopenight/entity/ProductImportHistory.java
@@ -0,0 +1,34 @@
+package com.piseth.java.school.phoneshopenight.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="import_histories")
+public class ProductImportHistory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="history_id")
+	private Long id;
+	@Column(name="import_unit")
+	private Integer importUnit;
+	@Column(name="unit_price")
+	private BigDecimal unitPrice;
+	@ManyToOne
+	@JoinColumn(name="product_id")
+	private Product product;
+	@Column(name="import_date")
+	private LocalDateTime importDate;
+}
