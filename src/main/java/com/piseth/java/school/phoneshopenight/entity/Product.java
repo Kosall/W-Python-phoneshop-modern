a5a31064d3178dc5719f508ddc5f531b99a28fe2
Kosal Nim <kosall123nim@gmail.com>
2024-12-08 19:04:36 +0700

    create Product.java

---
+++ src/main/java/com/piseth/java/school/phoneshopenight/entity/Product.java
@@ -0,0 +1,37 @@
+package com.piseth.java.school.phoneshopenight.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="products")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="product_id")
+	private Long id;
+	@Column(name="product_name")
+	private String name;
+	@Column(name="unit")
+	private int avalabeUnit;
+	@ManyToOne
+	@JoinColumn(name="model_id")
+	private Model model;
+	@ManyToOne
+	@JoinColumn(name="colour_id")
+	private Colour colour;
+	@Column(name="sale_price")
+	private BigDecimal salePrice;
+
+}
