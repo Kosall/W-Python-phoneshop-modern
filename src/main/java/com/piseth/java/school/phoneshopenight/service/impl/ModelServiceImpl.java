da61292b810ad9a7fe3c1cef7a421d024ad899a0
Kosal Nim <kosall123nim@gmail.com>
2024-12-11 13:06:49 +0700

    create ModelServiceImpl.java

--- src/main/java/com/piseth/java/school/phoneshopenight/service/impl/ModelServiceImpl.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/impl/ModelServiceImpl.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.piseth.java.school.phoneshopenight.entity.Model;
+import com.piseth.java.school.phoneshopenight.exception.ResourceNotFoundException;
+import com.piseth.java.school.phoneshopenight.repository.BrandRepository;
 import com.piseth.java.school.phoneshopenight.repository.ModelRepository;
+import com.piseth.java.school.phoneshopenight.service.BrandService;
 import com.piseth.java.school.phoneshopenight.service.ModelService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class ModelServiceImpl implements ModelService {
 	private final ModelRepository modelRepository;
+	private final BrandService brandService;
 
 	@Override
 	public Model save(Model model) {
@@ -23,6 +27,20 @@ public class ModelServiceImpl implements ModelService {
 	@Override
 	public List<Model> getByBrand(Long brandId) {
 		return modelRepository.findByBrandId(brandId);
+	}
+
+	@Override
+	public List<Model> getModelByBrandId(Long id) {
+		// TODO Auto-generated method stub
+		brandService.getById(id);
+		List<Model> list = modelRepository.findByBrandId(id);
+		return list;
+	}
+
+	@Override
+	public Model getById(Long id) {
+		// TODO Auto-generated method stub
+		return modelRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Model", id));
 	}
 
 }
