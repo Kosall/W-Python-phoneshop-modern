856fa8a092f0586f05a282a228c128a5f4ab1abf
Kosal Nim <kosall123nim@gmail.com>
2024-12-11 13:12:17 +0700

    create Brand.java

--- src/main/java/com/piseth/java/school/phoneshopenight/entity/Brand.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/entity/Brand.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -14,8 +15,9 @@ import lombok.Data;
 @Table(name = "brands")
 public class Brand {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "brand_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY/*generator = "mySeqGen"*/)
+	//@SequenceGenerator(name="mySeqGen",sequenceName = "brands_brand_id_seq",initialValue = 1,allocationSize = 1)
+	@Column(name = "brand_id"/*,updatable = false,nullable = false*/)
 	private Long id;
 	
 	@Column(name = "brand_name")
