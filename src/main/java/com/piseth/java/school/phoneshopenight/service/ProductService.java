c5b9b6b8f49b3495b77727a8d4a7fffbea6581d1
Kosal Nim <kosall123nim@gmail.com>
2024-12-11 20:35:29 +0700

    add importProduct() method in ProductService.java

--- src/main/java/com/piseth/java/school/phoneshopenight/service/ProductService.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.piseth.java.school.phoneshopenight.service;
 
+import com.piseth.java.school.phoneshopenight.dto.ProductImportDTO;
 import com.piseth.java.school.phoneshopenight.entity.Product;
 
 public interface ProductService {
 	Product create(Product product);
+	void importProduct(ProductImportDTO importDTO);
+	Product getById(Long id);
 
 }
