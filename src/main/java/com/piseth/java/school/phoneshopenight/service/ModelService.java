70f0bb97f6bd22140733d06c199907f9a6467782
Kosal Nim <kosall123nim@gmail.com>
2024-12-11 13:08:43 +0700

    change some in ModelService.java

--- src/main/java/com/piseth/java/school/phoneshopenight/service/ModelService.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/ModelService.java
@@ -7,5 +7,6 @@ import com.piseth.java.school.phoneshopenight.entity.Model;
 public interface ModelService {
 	Model save(Model model);
 	List<Model> getByBrand(Long brandId);
-	
+	List<Model>getModelByBrandId(Long id);
+	Model getById(Long id);
 }
