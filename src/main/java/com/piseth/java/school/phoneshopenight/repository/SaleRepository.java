3da60766d9a4358223e4037851170017afd302eb
Kosal Nim <kosall123nim@gmail.com>
2024-12-17 14:41:53 +0700

    create SaleRepository.java

---
+++ src/main/java/com/piseth/java/school/phoneshopenight/repository/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.piseth.java.school.phoneshopenight.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.piseth.java.school.phoneshopenight.entity.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
