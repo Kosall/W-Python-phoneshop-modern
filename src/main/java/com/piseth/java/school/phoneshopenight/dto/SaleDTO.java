484b5354ccfbcd4dbc2e892113418f838c4e0c29
Kosal Nim <kosall123nim@gmail.com>
2024-12-17 14:43:29 +0700

    create SaleDTO.java

---
+++ src/main/java/com/piseth/java/school/phoneshopenight/dto/SaleDTO.java
@@ -0,0 +1,16 @@
+package com.piseth.java.school.phoneshopenight.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+public class SaleDTO {
+	private List<ProductSellDTO> productions;
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime saleDate;
+
+}
