7de775f3a13b8cfb83c1612ccbcafc86bc9b3c5c
Kosal Nim <kosall123nim@gmail.com>
2024-12-17 14:46:56 +0700

    create ProductService.java

--- src/main/java/com/piseth/java/school/phoneshopenight/service/ProductService.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/ProductService.java
@@ -1,11 +1,16 @@
 package com.piseth.java.school.phoneshopenight.service;
 
+import java.math.BigDecimal;
+
 import com.piseth.java.school.phoneshopenight.dto.ProductImportDTO;
+import com.piseth.java.school.phoneshopenight.dto.SaleDTO;
 import com.piseth.java.school.phoneshopenight.entity.Product;
 
 public interface ProductService {
 	Product create(Product product);
 	void importProduct(ProductImportDTO importDTO);
 	Product getById(Long id);
+	void setSalePrice(Long productId,BigDecimal price);
+	void validateStocking(Long productId,Integer quantity);
 
 }
