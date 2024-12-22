bebf562e928b1285c77005ecdc45767c472963d2
Kosal Nim <kosall123nim@gmail.com>
2024-12-23 05:49:14 +0700

    create Optional<Product>findByModelIdAndColourId(Long modelId,Long colourId);in ProductRepository

--- src/main/java/com/piseth/java/school/phoneshopenight/repository/ProductRepository.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.piseth.java.school.phoneshopenight.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,5 @@ import com.piseth.java.school.phoneshopenight.entity.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>,JpaSpecificationExecutor<Product> {
-
+	Optional<Product>findByModelIdAndColourId(Long modelId,Long colourId); 
 }
