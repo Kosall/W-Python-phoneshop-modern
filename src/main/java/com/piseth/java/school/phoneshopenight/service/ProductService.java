df80f0c78d2995369b9ffe4f5d7257c8283fcff1
Kosal Nim <kosall123nim@gmail.com>
2024-12-23 05:43:46 +0700

    add upload(MultipartFile file) type void in ProductService.java

--- src/main/java/com/piseth/java/school/phoneshopenight/service/ProductService.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/ProductService.java
@@ -2,6 +2,8 @@ package com.piseth.java.school.phoneshopenight.service;
 
 import java.math.BigDecimal;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.piseth.java.school.phoneshopenight.dto.ProductImportDTO;
 import com.piseth.java.school.phoneshopenight.dto.SaleDTO;
 import com.piseth.java.school.phoneshopenight.entity.Product;
@@ -10,7 +12,9 @@ public interface ProductService {
 	Product create(Product product);
 	void importProduct(ProductImportDTO importDTO);
 	Product getById(Long id);
+	Product getByModelIdAndColourId(Long modelId,Long colourId);
 	void setSalePrice(Long productId,BigDecimal price);
 	void validateStocking(Long productId,Integer quantity);
+	void upload(MultipartFile file);
 
 }
