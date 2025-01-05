fd6f77c45819b61caff50a76013b027476802274
Kosal Nim <kosall123nim@gmail.com>
2025-01-05 13:26:31 +0700

    create ColourRepositoryTest.java

---
+++ src/test/java/com/piseth/java/school/phoneshopenight/repository/ColourRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.piseth.java.school.phoneshopenight.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class ColourRepositoryTest {
+	@Autowired
+	private ColourRespository colourRespository;
+
+}
