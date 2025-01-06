114b01e146e74ae9b51efc28571365fbaa14404b
Kosal Nim <kosall123nim@gmail.com>
2025-01-06 20:57:18 +0700

    add more methods in

--- src/main/java/com/piseth/java/school/phoneshopenight/service/SaleService.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/SaleService.java
@@ -1,7 +1,11 @@
 package com.piseth.java.school.phoneshopenight.service;
 
 import com.piseth.java.school.phoneshopenight.dto.SaleDTO;
+import com.piseth.java.school.phoneshopenight.entity.Sale;
 
 public interface SaleService {
 	void seller(SaleDTO saleDTO);
+	void cancelSale(Long saleId);
+	Sale getById(Long id);
+	void delete(Sale sale);
 }
