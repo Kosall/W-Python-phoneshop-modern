ee73df345df12dea4cf104d5cc1e3892627049a8
Kosal Nim <kosall123nim@gmail.com>
2025-01-10 21:27:27 +0700

    create ReportController.java and its methods

---
+++ src/main/java/com/piseth/java/school/phoneshopenight/controller/ReportController.java
@@ -0,0 +1,32 @@
+package com.piseth.java.school.phoneshopenight.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.piseth.java.school.phoneshopenight.projection.ProductSoldService;
+import com.piseth.java.school.phoneshopenight.service.ReportService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("reports")
+@RequiredArgsConstructor
+
+public class ReportController {
+	private final ReportService reportService;
+
+	@GetMapping("{startDate}/{endDate}")
+	public ResponseEntity<?> soldProduct( @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") @PathVariable("startDate")  LocalDateTime startDate,@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") @PathVariable("endDate") LocalDateTime endDate) {
+		List<ProductSoldService> products = reportService.getProductSoldService(startDate, endDate);
+
+		return ResponseEntity.ok(products);
+	}
+
+}
