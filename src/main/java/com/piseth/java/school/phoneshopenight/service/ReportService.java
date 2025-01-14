0b09801f5d644d17618b7748299c921ecb8235d1
Kosal Nim <kosall123nim@gmail.com>
2025-01-14 15:53:23 +0700

    add one method in ReportService.java

--- src/main/java/com/piseth/java/school/phoneshopenight/service/ReportService.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/ReportService.java
@@ -3,10 +3,12 @@ package com.piseth.java.school.phoneshopenight.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.piseth.java.school.phoneshopenight.dto.ProductReportDTO;
 import com.piseth.java.school.phoneshopenight.projection.ProductSoldService;
 
 public interface ReportService {
 	List<ProductSoldService> getProductSoldService(LocalDateTime startDate,LocalDateTime endDate);
+	List<ProductReportDTO> getReportProduct(LocalDateTime startDate,LocalDateTime endDate);
 	
 
 }
