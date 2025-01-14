3116332fb1c4aa200534c52621e4334c3fa662bb
Kosal Nim <kosall123nim@gmail.com>
2025-01-14 15:54:46 +0700

    add List<SaleDetail> findBySaleId(Long id) in SaleDetailRepository.java

--- src/main/java/com/piseth/java/school/phoneshopenight/repository/SaleDetailRepository.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/repository/SaleDetailRepository.java
@@ -3,14 +3,16 @@ package com.piseth.java.school.phoneshopenight.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.piseth.java.school.phoneshopenight.entity.Sale;
 import com.piseth.java.school.phoneshopenight.entity.SaleDetail;
 @Repository
-public interface SaleDetailRepository extends JpaRepository<SaleDetail, Long> {
+public interface SaleDetailRepository extends JpaRepository<SaleDetail, Long>,JpaSpecificationExecutor<SaleDetail>{
 	
 	List<SaleDetail> findBySaleId(Long id);
 
+	
 }
