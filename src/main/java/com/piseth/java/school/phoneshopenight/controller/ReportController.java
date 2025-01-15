337beaf99a11542037b530b97ffbdb99e96337ef
Kosal Nim <kosall123nim@gmail.com>
2025-01-15 21:31:11 +0700

    add one expenseReport(......) method in ReportController.java

--- src/main/java/com/piseth/java/school/phoneshopenight/controller/ReportController.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/controller/ReportController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.piseth.java.school.phoneshopenight.dto.ProductReportDTO;
+import com.piseth.java.school.phoneshopenight.dto.report.ExpenseReportDTO;
 import com.piseth.java.school.phoneshopenight.projection.ProductSoldService;
 import com.piseth.java.school.phoneshopenight.service.ReportService;
 
@@ -32,6 +33,12 @@ public class ReportController {
 	@GetMapping("2/{startDate}/{endDate}")
 	public ResponseEntity<?> soldProductV2( @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") @PathVariable("startDate")  LocalDateTime startDate,@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") @PathVariable("endDate") LocalDateTime endDate) {
 		List<ProductReportDTO> products = reportService.getReportProduct(startDate, endDate);
+
+		return ResponseEntity.ok(products);
+	}
+	@GetMapping("{startDate}/{endDate}/expense")
+	public ResponseEntity<?> expenseReport( @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") @PathVariable("startDate")  LocalDateTime startDate,@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") @PathVariable("endDate") LocalDateTime endDate) {
+		 List<ExpenseReportDTO> products = reportService.getReportAsExpense(startDate, endDate);
 
 		return ResponseEntity.ok(products);
 	}
