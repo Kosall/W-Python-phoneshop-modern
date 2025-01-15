805841c01f6bf8e4427640f7fa83511f67679674
Kosal Nim <kosall123nim@gmail.com>
2025-01-15 21:27:23 +0700

    implements getReportAsExpense(LocalDateTime startDate, LocalDateTime endDate)and customize it

--- src/main/java/com/piseth/java/school/phoneshopenight/service/ReportService.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/ReportService.java
@@ -4,11 +4,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.piseth.java.school.phoneshopenight.dto.ProductReportDTO;
+import com.piseth.java.school.phoneshopenight.dto.report.ExpenseReportDTO;
 import com.piseth.java.school.phoneshopenight.projection.ProductSoldService;
 
 public interface ReportService {
 	List<ProductSoldService> getProductSoldService(LocalDateTime startDate,LocalDateTime endDate);
 	List<ProductReportDTO> getReportProduct(LocalDateTime startDate,LocalDateTime endDate);
+	List<ExpenseReportDTO> getReportAsExpense(LocalDateTime startDate,LocalDateTime endDate);
 	
 
 }
