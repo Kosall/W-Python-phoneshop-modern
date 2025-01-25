d3edae870b74c9d42f81ff1290836067b7fec073
Kosal Nim <kosall123nim@gmail.com>
2025-01-25 14:10:57 +0700

    change some in this

--- src/main/java/com/piseth/java/school/phoneshopenight/config/security/SecurityConfigure.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/config/security/SecurityConfigure.java
@@ -1,10 +1,10 @@
 package com.piseth.java.school.phoneshopenight.config.security;
 
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -13,16 +13,30 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.piseth.java.school.phoneshopenight.config.based.secure.Permission;
+import com.piseth.java.school.phoneshopenight.config.based.secure.RoleEnum;
+
 @Configuration
+@EnableGlobalMethodSecurity(
+		prePostEnabled = true,
+		securedEnabled = true,
+		jsr250Enabled = true
+		)
 public class SecurityConfigure extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private PasswordEncoder encoder;
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http
+		.csrf().disable()
+		.authorizeRequests()
 		.antMatchers("/","css/**","js/**","index.html")
 		.permitAll()
-		.antMatchers("/brands").hasRole("ADMIN")		
+		//.antMatchers("/brands").hasRole("ADMIN")
+		.antMatchers("/colours").hasRole(RoleEnum.SALE.name())
+		//.antMatchers(HttpMethod.POST, "/brands").hasAuthority(Permission.BRAND_WRITE.getDescription())
+		//.antMatchers(HttpMethod.GET, "/brands").hasAuthority(Permission.BRAND_READ.getDescription())
+		//.antMatchers(HttpMethod.GET, "/models").hasAuthority(Permission.MODEL_READ.getDescription())
 		.anyRequest()
 		.authenticated()
 		.and()
@@ -35,13 +49,15 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
 	UserDetails build = User.builder()
 				.username("Vireak")
 				.password(encoder.encode("reaksa01"))
-				.roles("SALE")
+				//.roles("SALE")
+				.authorities(RoleEnum.ADMIN.getAuthorities())
 				.build();
 	//User user=new User("nary",encoder.encode("nary01"),Collections.emptyList());
 	UserDetails user = User.builder()
 				.username("Reaksa")
 				.password(encoder.encode("reaksa01"))
-				.roles("ADMIN")
+				//.roles("ADMIN")
+				.authorities(RoleEnum.SALE.getAuthorities())
 				.build();
 		UserDetailsService service=new InMemoryUserDetailsManager(user,build);
 		return service;
