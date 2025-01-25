e7695a3186a03a6f93a8bcbad0f17409b8c6b220
Kosal Nim <kosall123nim@gmail.com>
2025-01-25 14:10:00 +0700

    add @PreAuthorize(hasAuthority('brand:write')) on post method and @PreAuthorize(hasAuthority('brand:read')) on get method

--- src/main/java/com/piseth/java/school/phoneshopenight/controller/BrandController.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/controller/BrandController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,7 +36,7 @@ public class BrandController {
 	private final BrandService brandService;
 	private final ModelService modelService;
 	private final ModelEntityMapper modelMapper;
-	
+	@PreAuthorize("hasAuthority('brand:write')")
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<?> create(@RequestBody BrandDTO brandDTO) {
 		Brand brand = BrandMapper.INSTANCE.toBrand(brandDTO);
@@ -43,7 +44,7 @@ public class BrandController {
 		
 		return ResponseEntity.ok(BrandMapper.INSTANCE.toBrandDTO(brand));
 	}
-	
+	@PreAuthorize("hasAuthority('brand:read')")
 	@GetMapping("{id}")
 	public ResponseEntity<?> getOneBrand(@PathVariable("id") Long brandId){
 		Brand brand = brandService.getById(brandId);
