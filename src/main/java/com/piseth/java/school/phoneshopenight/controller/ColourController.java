928b1479775858917abd2fa40c13b8f89a0beda9
Kosal Nim <kosall123nim@gmail.com>
2025-01-25 14:08:01 +0700

    nothing to commit

--- src/main/java/com/piseth/java/school/phoneshopenight/controller/ColourController.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/controller/ColourController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ public class ColourController {
 	@Autowired
 	private final ColourService colourService;
 	@PostMapping
+	
 	public ResponseEntity<?> create(@RequestBody ColourDTO colourDTO){
 		Colour colour = ColourMapper.INSTANTIATION.toColour(colourDTO);
 		colour=colourService.build(colour);
