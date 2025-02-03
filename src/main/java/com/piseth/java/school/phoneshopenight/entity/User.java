f6c0c5e9f1bfc104baa4b2b0759e8f28f912c1b4
Kosal Nim <kosall123nim@gmail.com>
2025-02-03 13:45:50 +0700

    create User.java

--- src/main/java/com/piseth/java/school/phoneshopenight/entity/User.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/entity/User.java
@@ -1,16 +1,23 @@
 package com.piseth.java.school.phoneshopenight.entity;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
 
 import com.piseth.java.school.phoneshopenight.config.based.secure.RoleEnum;
 
 import lombok.Data;
+import net.bytebuddy.agent.builder.AgentBuilder.PoolStrategy.Eager;
 
 @Entity
 @Table(name="users")
@@ -23,8 +30,8 @@ public class User {
 	private String lastname;
 	private String username;
 	private String password;
-	@Enumerated(EnumType.STRING)
-	private RoleEnum role;
+	@ManyToMany(fetch = FetchType.EAGER)
+	private Set<Role> roles;
 	private boolean accountNonExpired;
 	private boolean accountNonLocked;
 	private boolean credentialsNonExpired;
