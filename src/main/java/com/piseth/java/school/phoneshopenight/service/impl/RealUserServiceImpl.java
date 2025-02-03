2800f7ccbb5fa16262ca14590d20ad1a03c624a3
Kosal Nim <kosall123nim@gmail.com>
2025-02-03 13:45:05 +0700

    change some in this

--- src/main/java/com/piseth/java/school/phoneshopenight/service/impl/RealUserServiceImpl.java
+++ src/main/java/com/piseth/java/school/phoneshopenight/service/impl/RealUserServiceImpl.java
@@ -1,14 +1,19 @@
 package com.piseth.java.school.phoneshopenight.service.impl;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import com.piseth.java.school.phoneshopenight.config.based.secure.UserAuthor;
 //import com.piseth.java.school.phoneshopenight.config.based.secure.UserAuthor;
 import com.piseth.java.school.phoneshopenight.config.security.service.UserService;
+import com.piseth.java.school.phoneshopenight.entity.Role;
 import com.piseth.java.school.phoneshopenight.entity.User;
 import com.piseth.java.school.phoneshopenight.exception.ApiException;
 import com.piseth.java.school.phoneshopenight.repository.UserRepository;
@@ -17,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 @Primary
 @Service
 @RequiredArgsConstructor
+
 public class RealUserServiceImpl implements UserService {
 	
 	private final UserRepository repository;
@@ -28,7 +34,7 @@ public class RealUserServiceImpl implements UserService {
 				UserAuthor author=UserAuthor.builder()
 						.username(users.getUsername())
 						.password(users.getPassword())
-						.authorities(users.getRole().getAuthorities())
+						.authorities(getAuthorities(users.getRoles()))
 						.accountNonExpired(users.isAccountNonExpired())
 						.accountNonLocked(users.isAccountNonLocked())
 						.credentialsNonExpired(users.isCredentialsNonExpired())
@@ -38,5 +44,20 @@ public class RealUserServiceImpl implements UserService {
 		return Optional.ofNullable(author);
 	
 	}
+	private Set<SimpleGrantedAuthority> getAuthorities(Set<Role> roling) {
+	
+		Set<SimpleGrantedAuthority> collect = roling.stream().map((r->new SimpleGrantedAuthority("ROLE_" + r))).collect(Collectors.toSet());
+		Set<SimpleGrantedAuthority> collect2 = roling.stream().flatMap(role->getStream(role)).collect(Collectors.toSet());
+		collect2.addAll(collect);
+		return collect2;	
+			
+		
+		
+		
+	}
+	private Stream<SimpleGrantedAuthority> getStream(Role re){
+		return re.getPermissions().stream().map(p->new SimpleGrantedAuthority(p.getName()));
+	}
+	
 
 }
